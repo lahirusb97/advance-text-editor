d500871ea5c4a654b9c65021477dd29d01d83c47
lahiru <lahirushirandev@gmail.com>
2023-07-03 22:31:32 +0530

    first commit

--- src/Components/TextEditor.jsx
+++ src/Components/TextEditor.jsx
@@ -2,27 +2,59 @@ import { TextField } from "@mui/material";
 import React, { useState } from "react";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import { EditorState } from "draft-js";
 import { convertFromRaw, convertToRaw } from "draft-js";
 import draftToHtml from "draftjs-to-html";
-// import htmlToDraft from "html-to-draftjs";
+import {
+  ChakraProvider,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
 export default function TextEditor() {
   const [editorState, seteditorState] = useState(EditorState.createEmpty());
-  console.log(convertToRaw(editorState.getCurrentContent()));
   const htmlContent = draftToHtml(
     convertToRaw(editorState.getCurrentContent())
   );
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <div className="shadow-lg w-full m-auto ">
+    <div className="shadow-lg w-full h-auto m-auto ">
+      <button
+        onClick={onOpen}
+        className="bg-gray-900 py-2 px-4 font-semibold text-white m-auto block"
+      >
+        Preview
+      </button>
       <Editor
-        toolbarClassName="flex sticky top-0 z-50 !justify-center mx-auto"
-        editorClassName=" bg-white shadow-lg max-w-5xl mx-auto border p-10"
+        toolbarClassName="flex sticky top-0 z-40 justify-center mx-auto"
+        editorClassName="bg-gray-200 mx-4 shadow-lg h-auto min-h-ww mb-4"
         onEditorStateChange={(e) => seteditorState(e)}
         editorState={editorState}
       />
-      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+
+      <ChakraProvider>
+        <Modal size={"full"} isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <div className="m-4 z-50" style={{ width: "100%" }}>
+                <div
+                  dangerouslySetInnerHTML={{ __html: htmlContent }}
+                  style={{ width: "100%", overflowX: "auto" }}
+                ></div>
+              </div>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      </ChakraProvider>
     </div>
   );
 }
